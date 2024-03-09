f3cf8df2c5e6437f3f28c776829faeed33a00247
dathbe <github@beffa.us>
2024-03-09 14:35:01 -0800

    Update MMM-GoogleMaps-Tracking.js
    again

--- MMM-GoogleMaps-Tracking.js
+++ MMM-GoogleMaps-Tracking.js
@@ -89,8 +89,8 @@ Module.register("MMM-GoogleMaps-Tracking",{
         mapElement.id = self.mapId;
         mapElement.classList.add("map-canvas");
         wrapper.appendChild(mapElement);
-        self.mapElement.style.height = "400px";
-        self.mapElement.style.width = "300px";
+        mapElement.style.height = "400px";
+        mapElement.style.width = "300px";
         
 
         function calculateCenter(){
